e6c14ade02b3d0867a226c2f286c0376c9612638
Pavel Stepanov <Pavel.Stepanov@paf.com>
2018-08-08 12:32:09 +0300

    Adding first controller and first view, which visualize date and time only.

---
+++ src/main/java/fi/candysoft/controller/HomeController.java
@@ -0,0 +1,13 @@
+package fi.candysoft.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
